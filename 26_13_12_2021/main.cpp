9ea197b155b6d9f25a4d5b6274958966cd6f3d77
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 01:45:14 +0300

    Update main.cpp

--- 26_13_12_2021/main.cpp
+++ 26_13_12_2021/main.cpp
@@ -515,77 +515,76 @@ BİR ÖRNEK DAHA
 2. Sadece id 
 3. İkisinin birlikte olması özellikleri olacak
 
-class Person{
+#include <iostream>
+#include <format>
+#include <string>
+#include <vector>
+
+
+class Person {
 public:
 	Person(std::string name, int id) : m_name(std::move(name)), m_id(id) { }
-	
-	//sınıfın get funcları olacak
-	std::string get_name()const{ return m_name;}
-	int get_id()const{ return m_id;}
 
+	//sınıfın get funcları olacak
+	std::string get_name()const { return m_name; }
+	int get_id()const { return m_id; }
 private:
 	std::string m_name;
 	int m_id;
 };
 
 template<>
-class std::formatter<Person>{
+class std::formatter<Person> {
 public:
-	constexpr auto parse(auto &context)
+	constexpr auto parse(auto& context)
 	{
-		auto iter{context.begin()};
+		auto iter{ context.begin() };
 		const auto iter_end{ context.end() };
-
-		if(iter == iter_end || *iter == '}') // Eğer birşey girilmemişse default olma durumu
+		if (iter == iter_end || *iter == '}') // Eğer birşey girilmemişse default olma durumu
 		{
 			m_ftype = FormatType::All;
 			return iter;
 		}
-
-		switch(*iter)
+		switch (*iter)
 		{
 			//a all için, isim için n, id için i olacak.
-			case 'n' : f_type = FormatType::Name; break; 
-			case 'i' : f_type = FormatType::id; break; 
-			case 'a' : f_type = FormatType::All; break; 
-			default : throw std::format_error{"Person format error!"};
+		case 'n': m_ftype = FormatType::Name; break;
+		case 'i': m_ftype = FormatType::Id; break;
+		case 'a': m_ftype = FormatType::All; break;
+		default: throw std::format_error{ "Person format error!" };
 		}
-		
-		++iter;
 
-		if(iter != iter_end && *iter != '}') // Burası hata durumu
+		++iter;
+		if (iter != iter_end && *iter != '}') // Burası hata durumu
 		{
-			throw std::format_error{"Person format error!"};
+			throw std::format_error{ "Person format error!" };
 		}
 		return iter;
 	}
-
-	constexpr auto format(const Person &per, auto &context)
+	constexpr auto format(const Person& per, auto& context)
 	{
 		using enum FormatType; // FormatType::name yerine doğrudan name kullanabilşirim artık.İsimleri görünür yaptık.
-		switch(m_ftype)
+		switch (m_ftype)
 		{
-			case Name : return std::format_to(context.out(), "{}",per.get_name());
-			case Name : return std::format_to(context.out(), "{}",per.get_id());
-			case Name : return std::format_to(context.out(), "[{}, {}]",per.get_id(),per.get_name());
+		case Name: return std::format_to(context.out(), "{}", per.get_name());
+		case Id: return std::format_to(context.out(), "{}", per.get_id());
+		case All: return std::format_to(context.out(), "[{}, {}]", per.get_id(), per.get_name());
 		}
 	}
-
 private:
 	//Formatlamadaki 3 seçeneği tutmak için bir enum türü oluşturuyoruz
-	enum class FormatType{Name, Id, All};
-
+	enum class FormatType { Name, Id, All };
 	FormatType m_ftype;
 };
 
 int main()
 {
-	Person p1{"necati",876};
-	Person p2{"harun",567};
-	Person p2{"murat",789};
-
-	std::cout << std::format("{:n}\n{:i}\n{:a}",p1,p2,p3);
+	Person p1{ "necati",876 };
+	Person p2{ "harun",567 };
+	Person p3{ "murat",789 };
+	std::cout << std::format("{:n}\n{:i}\n{:a}", p1, p2, p3);
 }
+
 necati
 567
 [murat,789]
