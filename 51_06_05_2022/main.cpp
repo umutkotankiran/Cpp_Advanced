bda6d93ee4547584132e1e5718b0f8b89954544a
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-05-11 15:18:19 +0300

    Update main.cpp

--- 51_06_05_2022/main.cpp
+++ 51_06_05_2022/main.cpp
@@ -301,10 +301,13 @@ std::lock_guard
 sınıfın kurcu işlevi mutex'i kilitliyor. Sınıfın sonlandırıcı işlevi kilidi açıyor.
 
 std::scoped_lock
-std::lock_guard sınıfına benziyor. Ancak sınıfın kurucu işlevi birden fazla mutex nesnesini alabiliyor. Kurucu işlev aldığı sırayla mutex nesnelerini kilitliyor. Sonlandırıcı işlev ters sırada kilitleri açıyor.
+std::lock_guard sınıfına benziyor. Ancak sınıfın kurucu işlevi birden fazla mutex nesnesini alabiliyor. 
+Kurucu işlev aldığı sırayla mutex nesnelerini kilitliyor. Sonlandırıcı işlev ters sırada kilitleri açıyor.
 
 std::unique_lock
-Locks a mutex in exclusive mode. The constructor also accepts arguments that instruct it to timeout instead of blocking forever on the lock call. It is also possible to not lock the mutex at all, or to assume that it is locked already, or to only try locking the mutex. Additional methods allow to lock and unlock the mutex during the unique_lock lock’s lifetime.
+Locks a mutex in exclusive mode. The constructor also accepts arguments that instruct it to timeout instead of blocking forever on the lock call. 
+It is also possible to not lock the mutex at all, or to assume that it is locked already, or to only try locking the mutex. 
+Additional methods allow to lock and unlock the mutex during the unique_lock lock’s lifetime.
 
 std::shared_lock.
 Same as unique_lock, but all operations are applied on the mutex in shared mode.
