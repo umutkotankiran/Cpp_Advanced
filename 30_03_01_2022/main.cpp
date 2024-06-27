c51d3736aa7d3b53be82fa249477adf74e2a1a6c
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2024-06-27 15:46:33 +0300

    Update main.cpp

--- 30_03_01_2022/main.cpp
+++ 30_03_01_2022/main.cpp
@@ -186,6 +186,24 @@ int main()
 
 int main()
 {
+    using namespace std;
+
+    vector source = { 4, 3, 6, 2, 12, 7, 9, 24, 15, 7, 21 };
+    auto v = source
+        | views::filter([](const int n) {return n % 3 == 0; })
+        | views::transform([](const int n) {return n * n; })
+        | ranges::to<vector>();
+
+    for (auto i : v)
+        cout << i << ' ';
+}
+
+---------------------------------------------------------------------------------------------------------
+
+ÖR:
+
+int main()
+{
 	using namespace std;
 
 	vector ivec{12, 5, 218, 9, 3, 24, -1, -9, -41, 6};
